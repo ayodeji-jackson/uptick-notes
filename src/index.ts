04d521c07ebd0610673eff776964bc136623c902
Ayodeji Jackson Oyeleye <emmanueljackie01@gmail.com>
2023-02-17 20:30:02 +0100

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Application } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT: string | number = process.env.PORT || 3000;
+const mongoString = process.env.DATABASE_URL;
+
+app.use(express.json());
+
+mongoose.connect(mongoString!);
+mongoose.connection.on('error', err => console.log(err));
+mongoose.connection.once('connected', () => console.log('database connected'));
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`)
+});
